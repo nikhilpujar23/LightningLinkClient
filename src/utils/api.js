8c426b25a46d837d3ed39673e89c258c73f39f39
Nikhil <nikhilpujar23@gmail.com>
2023-07-08 15:41:58 +0530

    commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,11 +3,11 @@ import axios from "axios";
 const serverApi = process.env.REACT_APP_SERVER;
 
 export const getRoomExists = async (roomId) => {
-  const response = await axios.get(`${serverApi}/room-exists/${roomId}`);
+  const response = await axios.get(`${serverApi}/api/room-exists/${roomId}`);
   return response.data;
 };
 
 export const getTURNCredentials = async () => {
-  const response = await axios.get(`${serverApi}/get-turn-credentials`);
+  const response = await axios.get(`${serverApi}/api/get-turn-credentials`);
   return response.data;
 };
